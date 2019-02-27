c5165178ddf59461eb49b20f1a2a554708982197
Julien Lebunetel <julien@lebunetel.com>
2019-02-27 14:13:36 +0100

    Add Arduino sketch

---
+++ code/signal_brut/signal_brut.ino
@@ -0,0 +1,21 @@
+
+/*
+   Un simple sketch permettant de lire la valeur du capteur IR sur
+   le terminal série ou le traceur série de l'IDE Arduino.
+*/
+
+// Le capteur IR est branché sur le port analogique
+#define analogPin A0
+
+void setup() {
+  // activation de la liaison série
+  Serial.begin(115200);
+}
+
+void loop() {
+  // on envoie directement sur la liaison série la valeur brut du capteur
+  Serial.println(analogRead(analogPin));
+  
+  // on temporise un peu
+  delay(20);
+}
